72f7c12941eb8e63cea73ee08292fefd2d7ae697
suren <spsurenpradhan@gmail.com>
2020-06-15 08:46:46 -0600

    done

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -3,22 +3,26 @@
 // Create a variable called 'myName' with a value that is your name. 
 
 // Code here
-
+var myName = "suren";
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
 //Code here
-
+var faveNum = 30;
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
+var lovesCode = true;
 
 //Code here
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
+function sum(num1,num2){
+	return num1 + num2
+}
 
 //Code here
 
@@ -46,10 +50,11 @@
 const faveColors = ['red', 'green', 'black']
 
 //Code Here
-
+let colorCopy = faveColors.slice(0,3);
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push.
+colorCopy.push('blue')
 
 //Code Here
 
@@ -57,15 +62,23 @@ const faveColors = ['red', 'green', 'black']
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
-
+var middleNums = numbers.slice(1,4);
 //Code Here
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has the following keys: firstName, state, age, and greeter. The value of the firstName key should be your name as a string. The value of the property state should be your current state or providence of residence as a string. The value of age should be your age as a number. greeter should be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the corresponding values. 
 
-//Code Here
+//Code Here 
+var me ={
+	name:"suren",
+	state:"utah",
+	age:30,
+	greeter: function(){
+		return 
 
+	}
+}
 //////////////////PROBLEM 12////////////////////
 
 // Create a function called 'bigOrSmall' that takes in one parameter, 'arr', which will be an array of numbers. Inside of the bigOrSmall function, create a new array called 'answers'. Then, loop over the passed in arr parameter, and check to see if the number in the array is GREATER than 100. If it is, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function.
@@ -87,32 +100,36 @@ const myNumbers = [3, 55, 788, 2, 1]
 //////////////////PROBLEM 14////////////////////
 
 // First, use .map to create a new array that doubles each number. Call the new array 'doubled'.
-
+let doubled = myNumbers.map( x=>x*2)
 // Code Here
 
 //////////////////PROBLEM 15////////////////////
 
 // Now, use .filter to create a new array containing any numbers that are greater than 100. Call the new array 'filtered'. 
-
+let filter = myNumbers.filter(myNumbers=>myNumbers.length>100);
 // Code Here
 
 //////////////////PROBLEM 16////////////////////
 
 // Next, use reduce to get the total of the numbers in the array summed together. Store the total in a new variable named 'total'. 
+let  total = myNumbers.reduce(accmulater,currentValue) => accmulater + currentValue;
 
 // Code Here
 
 //////////////////PROBLEM 17////////////////////
 
 // Finally, use .forEach to find the index of each item in the array. To begin, create an empty array called 'myNumbersIndex'. Then, use forEach to push each item's index from the myNumbers array into the newly created myNumbersIndex array. 
-
+let myNumbersIndex = []
+for( let i = 0; i <myNumbers.length ; i ++){
+myNumbersIndex.push(myNumbers[i]);
+}
 // Code Here
 
 //////////////////PROBLEM 18////////////////////
 
 // Did you know that George Foreman has five sons named George? Go ahead and change everyone's name in the notGeorge array to George using .map. Store the resulting array in a variable named 'forTheLoveOfGeorge'.
 const notGeorge = ['Louis', 'Ted', 'Bill', 'Sharon', 'Mark', 'Angela']
-
+let George = notGeorge.map ( x => x)
 // Code Here
 
 //////////////////PROBLEM 19////////////////////
